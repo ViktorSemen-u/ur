627c15db6907cc7cc05ba37726c214cd637fcd55
my <my@example.com>
2025-05-08 00:03:20 +0300

    Base Tables

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Home;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,8 +17,23 @@ class HomeController extends AbstractController
      */
     public function index(): Response
     {
+        // $home = new Home();
+        // $home->setTitle("Привіт")
+        //     ->setContent("Текст привітання");
+
+        // $entityManager = $this->getDoctrine()->getManager();
+        // $entityManager->persist($home);
+        // $entityManager->flush();
+
+        // $home = $this->getDoctrine()->getRepository(Home::class)->findOneBy(array("id"=> 1));
+        $home = $this->getDoctrine()->getRepository(Home::class)->find(1);
+        // dd($home);
+        // dump($home).die;
+        // dump($home);
+        // var_dump($home);
         return $this->render('home/index.html.twig', [
-            'user_name' => 'не зреєстрований'
+            'user_name' => 'не зреєстрований',
+            'home'=> $home,
         ]);
     }
 }
